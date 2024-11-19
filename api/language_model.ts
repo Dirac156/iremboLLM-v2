b717140ef9c203a4e12e170acc29e047054c6778
dirac156 <dmurairimukongya@gmail.com>
2024-11-19 15:44:25 +0200

    fix sign in issue'

--- api/language_model.ts
+++ api/language_model.ts
@@ -4,7 +4,9 @@ import axios from "axios";
 class LanguageModelAPI {
   async generate(query: Query) {
     try {
-      const response = await axios.post("http://0.0.0.0:8000/agent", query);
+      const response = await axios.post("http://0.0.0.0:8000/agent", query, {
+        withCredentials: true,
+      });
       return response.data as Response;
     } catch (error) {
       console.error(error);
